42116867740a4d2abca3d16bc01210875a04cca0
Perttu Lähteenlahti <perttu@lahteenlahti.com>
2024-11-20 16:05:46 +0200

    chore: rename to be able to publish

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "plahteenlahti:factory",
+  name: "plahteenlahti:factory-next",
   summary: "Factories for Meteor, fork of dburles:factory",
   version: "1.5.1",
   git: "https://github.com/plahteenlahti/meteor-factory.git",
